4c4d6da7d365ae4f6ba0eb009eacf09e3893c785
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-12-04 18:07:23 +0100

    set context api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import './index.css';
+import React, { useState } from "react";
+import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./Pages/Index"
-import Login from "./Pages/Login"
-import Register from "./Pages/Register"
+import Index from "./Pages/Index";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Context from "./Context";
 
 function App() {
+  const [state, setState] = useState({});
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Index />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <Context.Provider value={{ state, setState }}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Index />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </Context.Provider>
   );
 }
 
 export default App;
-
