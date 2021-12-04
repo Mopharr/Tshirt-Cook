ec5ac5406eb6c8a0afad4fdf5f9f05993ec107ae
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-12-04 17:55:54 +0100

    login and register route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './index.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./Pages/Index"
-import L
+import Login from "./Pages/Login"
+import Register from "./Pages/Register"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Index />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
