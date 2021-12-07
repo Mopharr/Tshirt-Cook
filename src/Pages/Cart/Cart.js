123f55b2c0d7c631f17fe1613f6aad2493a099d2
youngengineer201@gmail.com <youngengineer201@gmail.com>
2021-12-06 17:57:05 -1200

    cart

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { FiSearch } from "react-icons/fi";
+import { FiSearch, FiX } from "react-icons/fi";
 import { BsCart3 } from "react-icons/bs";
 
 const Cart = () => {
@@ -38,7 +38,84 @@ const Cart = () => {
       <CartSection>
         <div className="cart-wrap">
           <div className="cart-placeholder">
-            <h2>Your shopping cart</h2>
+            <div>
+              <h2>Your shopping cart</h2>
+              <div className="seperator">
+                <div className="dark-line"></div>
+              </div>
+            </div>
+            <span>
+              <div className="cart-content">
+                <div>
+                  <div className="cart-content-header">
+                    <div className="cart-content-header-wrapper">
+                      <div className="left">
+                        <div className="inner">
+                          <span className="inner-title">Item</span>
+                        </div>
+                      </div>
+                      <div className="right">
+                        <div className="inner"></div>
+                        <div className="inner" id="qty">
+                          <span className="inner-title">Qty</span>
+                        </div>
+                        <div className="inner">
+                          <span className="inner-title">Price</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="cart-content-header">
+                    <div className="">
+                      <div className="">
+                        <div className="">
+                          <div className="">
+                            <div className="">
+                              <a href="#img">
+                                <img
+                                  src="https://ih1.redbubble.net/image.437353179.6883/ssrco,mhoodie,mens,101010:01c5ca27c6,back,square_product,x600-bg,f8f8f8.1u1.jpg"
+                                  alt="hoddie"
+                                />
+                              </a>
+                            </div>
+                          </div>
+                          <div className="">
+                          <div className="">
+                            <h2>Hoodie (Pullover)</h2>
+                            <p>L.Black, Black</p>
+                          </div>
+                          <div className="">
+                            <div className="">
+                                <div className="">
+                                    <span>-</span>
+                                    <input type='number' />
+                                    <span>+</span>
+                                </div>
+                            </div>
+                          </div>
+                          <div className="">
+                            <div className="">
+                                <div className="">
+                                    <span>$48.56</span>
+                                </div>
+                            </div>
+                          </div>
+                          <div className="">
+                            <div className="">
+                                <span>
+                                    <FiX />
+                                </span>
+                            </div>
+                          </div>
+                          </div>
+                          <div className=""></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </span>
           </div>
         </div>
       </CartSection>
@@ -150,5 +227,91 @@ const CartSection = styled.div`
   .cart-placeholder {
     display: flex;
     flex-direction: column;
+
+    h2 {
+      text-align: center;
+      font-size: 1.25rem;
+      font-weight: 500;
+      color: #303238;
+      line-height: 1.2;
+      padding: 16px;
+    }
+
+    .seperator {
+      background-color: inherit;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      width: 100%;
+      position: relative;
+      padding: 12px 0;
+
+      .dark-line {
+        background-color: #d6dadf;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 66%;
+        height: 1px;
+        margin: 0;
+        z-index: 1;
+        display: flex;
+        justify-content: center;
+      }
+    }
+
+    .cart-content {
+      transition: opacity 0.3s linear;
+
+      .cart-content-header {
+        max-width: 768px;
+        width: 100%;
+        margin: 0 auto;
+
+        .cart-content-header-wrapper {
+          display: flex;
+
+          .left {
+            display: flex;
+            flex: 1;
+            padding: 16px;
+          }
+
+          .right {
+            display: flex;
+            flex: 5;
+            flex-direction: row;
+            padding: 16px;
+          }
+
+          .inner {
+            display: flex;
+            flex-direction: column;
+            flex-grow: 1;
+            flex-basis: 0;
+            align-items: center;
+            justify-content: flex-start;
+
+            :nth-child(1) {
+              flex: 2;
+            }
+
+            .inner-title {
+              font-size: 0.875rem;
+              text-transform: uppercase;
+              color: #8c95a5;
+              line-height: 1;
+              font-weight: 600;
+            }
+
+            #qty {
+              flex: 0 0 144px;
+            }
+          }
+        }
+      }
+    }
   }
 `;
