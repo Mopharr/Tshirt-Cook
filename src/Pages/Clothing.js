025666d295ab7a5c62228397e6613c7242bbf45e
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-12-17 15:01:10 +0100

    commit

--- src/Pages/Clothing.js
+++ src/Pages/Clothing.js
@@ -1,26 +1,26 @@
-import Nav from 'components/Nav'
-import Section from 'components/Section'
-import  ClothHero from "components/Clothing/ClothHero";
-import React from 'react'
-import styled from 'styled-components'
-import ClothExplore from 'components/Clothing/ClothExplore';
-import Shop from 'components/Clothing/Shop';
-import Details from 'components/Clothing/Details';
-import Footer from 'components/Footer';
+import Nav from "components/Nav";
+import Section from "components/Section";
+import ClothHero from "components/Clothing/ClothHero";
+import React from "react";
+import styled from "styled-components";
+import ClothExplore from "components/Clothing/ClothExplore";
+import Shop from "components/Clothing/Shop";
+import Details from "components/Clothing/Details";
+import Footer from "components/Footer";
 
 const Clothing = () => {
-    return (
-        <Main>
-            <Nav />
-            <Section />
-            <ClothHero />
-            <ClothExplore />
-            <Shop />
-            <Details />
-            <Footer />
-        </Main>
-    )
-}
+  return (
+    <Main>
+      <Nav />
+      <Section />
+      <ClothHero />
+      <ClothExplore />
+      <Shop />
+      <Details />
+      <Footer />
+    </Main>
+  );
+};
 
-export default Clothing
-const Main = styled.div``
+export default Clothing;
+const Main = styled.div``;
