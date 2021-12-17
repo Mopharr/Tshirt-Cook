0ec31c6a7ef9c64362e21b03c8c587e7a0f0cf2e
quadri salawudeen <olamiquadri1@gmail.com>
2021-12-18 00:42:29 +0100

    clothing page

--- src/Pages/Clothing.js
+++ src/Pages/Clothing.js
@@ -2,16 +2,18 @@ import Nav from 'components/Nav'
 import React from 'react'
 import styled from 'styled-components'
 import ClothExplore from 'components/Clothing/ClothExplore';
-import Shop from 'components/Clothing/Shop';
 import Details from 'components/Clothing/Details';
 import Footer from 'components/Footer';
+import Hero from 'components/Hero';
+import Discover from 'components/Discover';
 
 const Clothing = () => {
     return (
         <Main>
             <Nav />
+            <Hero />
             <ClothExplore />
-            <Shop />
+            <Discover />
             <Details />
             <Footer />
         </Main>
