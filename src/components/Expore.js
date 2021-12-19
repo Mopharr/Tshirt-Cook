e64a97ca85fc7da943becefbd5bfe00f83d3f21a
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-12-19 12:21:53 +0100

    COMMIT

--- src/components/Expore.js
+++ src/components/Expore.js
@@ -17,31 +17,29 @@ const Expore = () => {
         </Text>
         <div class="containers">
           {product.map((item) => {
-            return (
-              <div class="card-1 card-div">
-                <div class="like-icon-div">
-                  <label for="card-1-like" class="like-icon-div-child">
-                    <input type="checkbox" id="card-1-like" />
-                    <AiOutlineHeart />
-                  </label>
-                </div>
+            <div class="card-1 card-div">
+              <div class="like-icon-div">
+                <label for="card-1-like" class="like-icon-div-child">
+                  <input type="checkbox" id="card-1-like" />
+                  <AiOutlineHeart />
+                </label>
+              </div>
 
-                <div class="gow-img-div img-div">
-                  <img src={item.media.source} alt="" />
-                </div>
-                <div class="text-container">
-                  <h2 class="item-name">{item.name}</h2>
-                  <div class="pricing-and-cart">
-                    <div class="pricing">
-                      <p class="current-price">$599</p>
-                    </div>
-                    <AiOutlineShoppingCart
-                      style={{ fontSize: "1.4em", cursor: "pointer" }}
-                    />
+              <div class="gow-img-div img-div">
+                <img src={item.media.source} alt="" />
+              </div>
+              <div class="text-container">
+                <h2 class="item-name">{item.name}</h2>
+                <div class="pricing-and-cart">
+                  <div class="pricing">
+                    <p class="current-price">$599</p>
                   </div>
+                  <AiOutlineShoppingCart
+                    style={{ fontSize: "1.4em", cursor: "pointer" }}
+                  />
                 </div>
               </div>
-            );
+            </div>;
           })}
 
           {/* <div class="card-2 card-div">
