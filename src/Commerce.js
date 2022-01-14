ad81e8199438ce16d2674fb51303b8d22ed1cce0
Olashina <69033389+olashina201@users.noreply.github.com>
2022-01-14 11:34:26 +0100

    commit

--- src/Commerce.js
+++ src/Commerce.js
@@ -1,3 +1,3 @@
 import Commerce from "@chec/commerce.js";
 
-export const commerce = new Commerce("sk_375891c7d6b3e834393b5240b8fa9466f5ac1b7540255", true);
+export const commerce = new Commerce("pk_375897dc7fbf5660850ce120393b61ab01cc77a1d92dd", true);
--- src/components/product.js
+++ src/components/product.js
@@ -12,14 +12,13 @@ const Product = () => {
       <div className="proContent">
         <IoIosArrowBack className="icons" />
         <div className="proContentCap">
-          {product.map((item) => {
-            return (
+          {product.map((item) => (
               <div class="card-1 card-div">
                 <div class="like-icon-div">
                   <label for="card-1-like" class="like-icon-div-child"></label>
                 </div>
                 <div class="gow-img-div img-div">
-                  <img src={item.media.source} alt="" />
+                  <img src={item.image.url} alt="" />
                 </div>
                 <div class="text-container">
                   <h2 class="item-name">{item.name}</h2>
@@ -36,8 +35,8 @@ const Product = () => {
                   </div>
                 </div>
               </div>
-            );
-          })}
+            )
+          )}
         </div>
         <IoIosArrowForward className="icons" />
       </div>
