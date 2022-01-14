10f9461bb15ad28e44db65777493dfd3d9f31fd5
Olashina <69033389+olashina201@users.noreply.github.com>
2022-01-14 12:01:28 +0100

    commit

--- src/components/Checkout/PaymentForm.js
+++ src/components/Checkout/PaymentForm.js
@@ -11,44 +11,40 @@ const PaymentForm = ({
   shippingData,
   onCaptureCheckout,
 }) => {
-  // const handleSubmit = async (event, elements, stripe) => {
-  //   event.preventDefault();
-  //   const orderData = {
-  //     line_items: checkoutToken.live.line_items,
-  //     customer: {
-  //       firstname: shippingData.firstName,
-  //       lastname: shippingData.lastName,
-  //       email: shippingData.email,
-  //     },
-  //     shipping: {
-  //       name: "International",
-  //       street: shippingData.address1,
-  //       town_city: shippingData.city,
-  //       county_state: shippingData.shippingSubdivision,
-  //       postal_zip_code: shippingData.zip,
-  //       country: shippingData.shippingCountry,
-  //     },
-  //     fulfillment: { shipping_method: shippingData.shippingOption },
-  //   };
-
-  //   onCaptureCheckout(checkoutToken.id, orderData);
-
-  //   nextStep();
-  // };
+  
+  const orderData = {
+    line_items: checkoutToken.live.line_items,
+    customer: {
+      firstname: shippingData.firstName,
+      lastname: shippingData.lastName,
+      email: shippingData.email,
+    },
+    shipping: {
+      name: "International",
+      street: shippingData.address1,
+      town_city: shippingData.city,
+      county_state: shippingData.shippingSubdivision,
+      postal_zip_code: shippingData.zip,
+      country: shippingData.shippingCountry,
+    },
+    fulfillment: { shipping_method: shippingData.shippingOption },
+  };
 
   const publicKey = "pk_live_a07545d3858ad1c11108c5be493fbbf217a9885b";
-console.log(checkoutToken.live.subtotal.raw)
+
   const componentProps = {
     email: shippingData.email,
     amount: checkoutToken.live.subtotal.raw * 100,
     metadata: {
-      name: shippingData.firstName,
-      phone: shippingData.no,
+      data: orderData,
     },
     publicKey,
     text: "Pay Now",
-    onSuccess: () =>
-      alert("Thanks for doing business with us! Come back soon!!"),
+    onSuccess: () => {
+      onCaptureCheckout(checkoutToken.id, orderData);
+      nextStep();
+      alert("Thanks for doing business with us! Come back soon!!");
+    },
     onClose: () => alert("Wait! Don't leave :("),
   };
 
