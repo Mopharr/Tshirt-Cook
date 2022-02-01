a2265a3a91926d122cc562c183d96f0bca2de551
youngengineer201@gmail.com <youngengineer201@gmail.com>
2022-02-01 18:23:32 +0100

    carousel

--- src/components/product.js
+++ src/components/product.js
@@ -1,75 +1,49 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import UserContext from "Context";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import "../style/product.css";
+import Card from "./Card.js";
 
 const Product = () => {
+  const [index, setIndex] = useState(0);
   const { addToCart, product } = useContext(UserContext);
+
+  const slideLeft = () => {
+    if (index - 1 >= 0) {
+      setIndex(index - 1);
+    }
+  };
+
+  const slideRight = () => {
+    if (index + 1 <= product.length - 1) {
+      setIndex(index + 1);
+    }
+  };
+
   return (
     <div className="product">
       <h2 className="haha">Feature Product</h2>
       <div className="proContent">
-        <IoIosArrowBack className="icons" />
-        <div className="proContentCap">
-          {product.map((item) => (
-            <a href="/details" class="cards">
-              <div class="img-div">
-                <img src={item.image.url} alt="" />
-              </div>
-              <div class="text-container">
-                <h2 class="item-name">{item.name}</h2>
-                <div class="pricing-and-cart">
-                  <div class="pricing">
-                    <p className="price">
-                      Price: {item.price.formatted_with_symbol}
-                    </p>
-                  </div>
-                  <button
-                    style={{ fontSize: "1.4em", cursor: "pointer" }}
-                    onClick={() => {
-                      addToCart(item.id, 1);
-                    }}
-                  >
-                    Add to cart
-                  </button>
-                </div>
-              </div>
-            </a>
-          ))}
-          {product.map((item) => (
-            <a href="/details" class="cards">
-              <div class="img-div">
-                <img src={item.image.url} alt="" />
-              </div>
-              <div class="text-container">
-                <h2 class="item-name">{item.name}</h2>
-                <div class="pricing-and-cart">
-                  <div class="pricing">
-                    <p className="price">
-                      Price: {item.price.formatted_with_symbol}
-                    </p>
-                  </div>
-                  <button
-                    style={{ fontSize: "1.4em", cursor: "pointer" }}
-                    onClick={() => {
-                      addToCart(item.id, 1);
-                    }}
-                  >
-                    Add to cart
-                  </button>
-                </div>
-              </div>
-            </a>
-          ))}
+        <div className="card-container">
+        <div className="background-block"></div>
+          <IoIosArrowBack className="leftBtn" onClick={slideLeft} />
+          <IoIosArrowForward className="rightBtn" onClick={slideRight} />
+          {product.map((item, n) => {
+            let position =
+              n > index ? "nextCard" : n === index ? "activeCard" : "prevCard";
+            return (
+              <Card
+                {...item}
+                key={n}
+                cardStyle={position}
+                addToCart={addToCart}
+              />
+            );
+          })}
         </div>
-        <IoIosArrowForward className="icons" />
       </div>
     </div>
   );
 };
 
 export default Product;
-
-
-
-
